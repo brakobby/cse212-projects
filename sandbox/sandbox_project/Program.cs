01a43ee28617a29fde4acedc34aed9c7bb8cc2af
brakobby <brakobby454@gmail.com>
2026-01-09 20:13:49 +0000

    practice lesson

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -8,6 +8,66 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
-    }
+        // var numbers = new int[3];
+        // numbers[0] = 1;
+        // numbers[1] = 2;
+        // numbers[2] = 3;
+
+        // Console.WriteLine(numbers);
+
+        // var evenNumber = new[] {1,2,3};
+        // Console.WriteLine(evenNumber[2]);
+
+        // List in C#
+        // var myList = new List<int>();
+        // myList.Add(1);
+        // myList.Add(2);
+        // myList.Add(3);
+
+        // using collection initializer
+        // var myList2 = new List<int> {1,2,3};
+        // myList2.Insert(1, 10);
+        // Console.WriteLine(myList2[2]);
+        // var total = myList2.Count;
+        // Console.WriteLine(total);
+        // Console.WriteLine(myList2.Capacity);
+
+
+        // Looping Through List or Array
+
+        // var mainList = new List<int> {1,2,3,4,5,6,7,8,9,10};
+
+        // foreach(var item in mainList)
+        // {
+        //     // Console.WriteLine(item);
+        //     if (item % 2 == 0)
+        //     {
+        //         Console.WriteLine($"{item} is Even");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine($"{item} is Odd");
+        //     }
+        // }
+
+    //     for (var index = 0; index < mainList.Count; ++index)
+    //     {
+    //         Console.WriteLine(myList[index]);
+    //     }
+
+    //     // fo arrays
+    //     var arr = new int[] {1,2,3};
+
+    //     for (var index = 0; index < arr.Length; ++index)
+    //     {
+    //         Console.WriteLine(arr[index]);
+    //     }
+
+
+    // EVALUATING PERFORMANCE (BIG 0 NOTATION)
+
+    
+
+
+     }
 }
